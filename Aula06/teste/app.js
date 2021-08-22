1dee3901f1ff3e7882d5d943b507c0cb5f952e70
Silvano AP <silvanoeng@hotmail.com>
2021-08-22 16:08:17 -0300

    atividade aula 06

---
+++ Aula06/teste/app.js
@@ -0,0 +1,18 @@
+
+let title = document.querySelector('.title');
+
+let questao = document.querySelector('.questao');
+
+let opcao = document.querySelector('.opcao');
+
+title.classList.add('palha');
+
+title.classList.remove('title');
+
+questao.classList.add('milho');
+
+questao.classList.remove('questao');
+
+opcao.classList.add('sabugo');
+
+opcao.classList.remove('opcao');
