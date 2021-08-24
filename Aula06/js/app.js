b83757bdd7faa4d1d4310f7a4372081461b94f53
Silvano AP <silvanoeng@hotmail.com>
2021-08-24 09:56:11 -0300

    criando pastas

--- Aula06/js/app.js
+++ Aula06/js/app.js
@@ -5,7 +5,7 @@ const listaDeFelinos = [
         title: 'O tigre',
         nome: 'Tigre',
         id: 'tigre',
-        imgUrl: 'tigre.jpg',
+        imgUrl: './img/tigre.jpg',
         description: 'O Tigre (Panthera tigris) é uma das espécies da subfamília dos panterinos (família Felidae) pertencente ao gênero Panthera. Só é encontrado no continente asiático; É um predador carnívoro e é a maior espécie de felino do mundo, junto com o leão, podendo ambos atingir um tamanho comparável ao dos maiores fósseis de felinos.',
         createdAt: '2021-06-01T23:12:11.837Z'
     },
@@ -13,7 +13,7 @@ const listaDeFelinos = [
         title: 'O leão',
         nome: 'Leão',
         id: 'leao',
-        imgUrl: 'leao.jpg',
+        imgUrl: './img/leao.jpg',
         description: 'O leão (Panthera leo) é um mamífero carnívoro da família dos felídeos que é uma das cinco espécies do gênero Panthera. Leões selvagens vivem em populações cada vez mais dispersas e fragmentadas na África Subsaariana (com exceção das regiões de selva da costa atlântica e da Bacia do Congo) e em uma pequena área do noroeste da Índia',
         createdAt: '2021-05-05T23:12:11.837Z'
     },
@@ -21,7 +21,7 @@ const listaDeFelinos = [
         title: 'O leopardo',
         nome: 'Leopardo',
         id: 'leopardo',
-        imgUrl: 'leopardo.jpg',
+        imgUrl: './img/leopardo.jpg',
         description: 'O leopardo (Panthera pardus) é um mamífero carnívoro da família dos felídeos. Como três dos outros gatos do gênero Panthera: o leão, o tigre e a onça, eles se caracterizam por uma modificação no osso hióide que lhes permite rugir. É também conhecida como pantera marrom e, quando tem a pelagem totalmente escura, como pantera negra (melânica).',
         createdAt: '2021-05-05T23:12:11.837Z'
     },
@@ -29,7 +29,7 @@ const listaDeFelinos = [
         title: 'A pantera negra',
         nome: 'Pantera Negra',
         id: 'pantera-negra',
-        imgUrl: 'pantera-negra.jpg',
+        imgUrl: './img/pantera-negra.jpg',
         description: 'A pantera negra é uma variação negra (melanismo) de várias espécies de grandes felinos, principalmente o leopardo (Panthera pardus) e a onça pintada (Panthera onca). Mas deve-se ressaltar que não é uma espécie nova, nem mesmo uma subespécie, é simplesmente uma variação negra desses animais.',
         createdAt: '2021-04-09T23:12:11.837Z'
     },
@@ -37,7 +37,7 @@ const listaDeFelinos = [
         title: 'O jaguar',
         nome: 'Jaguar',
         id: 'jaguar',
-        imgUrl: 'jaguar.jpg',
+        imgUrl: './img/jaguar.jpg',
         description: 'O jaguar, jaguar ou yaguareté (Panthera onca) é um felino carnívoro da subfamília dos Panterinos e gênero Panthera. É a única das cinco espécies atuais desse gênero encontradas na América. É também o maior felino da América e o terceiro maior do mundo, depois do tigre (Panthera tigris) e do leão (Panthera leo).',
         createdAt: '2021-05-18T23:12:11.837Z'
     },
@@ -45,7 +45,7 @@ const listaDeFelinos = [
         title: 'O guepardo',
         nome: 'Guepardo',
         id: 'guepardo',
-        imgUrl: 'chita.jpg',
+        imgUrl: './img/chita.jpg',
         description: 'A chita (Acinonyx jubatus) 1 é um membro atípico da família dos felídeos. É o único representante vivo do gênero Acinonyx. Ele caça graças à sua visão e grande velocidade. É o animal terrestre mais rápido, atingindo velocidade máxima de 115 km / h em corridas de até quatrocentos ou quinhentos metros.',
         createdAt: '2021-05-22T23:12:11.837Z'
     },
