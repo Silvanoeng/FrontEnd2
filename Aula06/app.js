c351581a0ca42d5dd6fcba9c932ea3401c4f93e6
Silvano AP <silvanoeng@hotmail.com>
2021-08-23 21:41:27 -0300

    add links com id

--- Aula06/app.js
+++ Aula06/app.js
@@ -3,36 +3,42 @@
 const listaDeFelinos = [
     {
         title: 'O tigre',
+        id: 'Tigre',
         imgUrl: 'tigre.jpg',
         description: 'O Tigre (Panthera tigris) é uma das espécies da subfamília dos panterinos (família Felidae) pertencente ao gênero Panthera. Só é encontrado no continente asiático; É um predador carnívoro e é a maior espécie de felino do mundo, junto com o leão, podendo ambos atingir um tamanho comparável ao dos maiores fósseis de felinos.',
         createdAt: '2021-06-01T23:12:11.837Z'
     },
     {
         title: 'O leão',
+        id: 'Leão',
         imgUrl: 'leao.jpg',
         description: 'O leão (Panthera leo) é um mamífero carnívoro da família dos felídeos que é uma das cinco espécies do gênero Panthera. Leões selvagens vivem em populações cada vez mais dispersas e fragmentadas na África Subsaariana (com exceção das regiões de selva da costa atlântica e da Bacia do Congo) e em uma pequena área do noroeste da Índia',
         createdAt: '2021-05-05T23:12:11.837Z'
     },
     {
         title: 'O leopardo',
+        id: 'Leopardo',
         imgUrl: 'leopardo.jpg',
         description: 'O leopardo (Panthera pardus) é um mamífero carnívoro da família dos felídeos. Como três dos outros gatos do gênero Panthera: o leão, o tigre e a onça, eles se caracterizam por uma modificação no osso hióide que lhes permite rugir. É também conhecida como pantera marrom e, quando tem a pelagem totalmente escura, como pantera negra (melânica).',
         createdAt: '2021-05-05T23:12:11.837Z'
     },
     {
         title: 'A pantera negra',
+        id: 'Pantera Negra',
         imgUrl: 'pantera-negra.jpg',
         description: 'A pantera negra é uma variação negra (melanismo) de várias espécies de grandes felinos, principalmente o leopardo (Panthera pardus) e a onça pintada (Panthera onca). Mas deve-se ressaltar que não é uma espécie nova, nem mesmo uma subespécie, é simplesmente uma variação negra desses animais.',
         createdAt: '2021-04-09T23:12:11.837Z'
     },
     {
         title: 'O jaguar',
+        id: 'Jaguar',
         imgUrl: 'jaguar.jpg',
         description: 'O jaguar, jaguar ou yaguareté (Panthera onca) é um felino carnívoro da subfamília dos Panterinos e gênero Panthera. É a única das cinco espécies atuais desse gênero encontradas na América. É também o maior felino da América e o terceiro maior do mundo, depois do tigre (Panthera tigris) e do leão (Panthera leo).',
         createdAt: '2021-05-18T23:12:11.837Z'
     },
     {
         title: 'O guepardo',
+        id: 'Guepardo',
         imgUrl: 'chita.jpg',
         description: 'A chita (Acinonyx jubatus) 1 é um membro atípico da família dos felídeos. É o único representante vivo do gênero Acinonyx. Ele caça graças à sua visão e grande velocidade. É o animal terrestre mais rápido, atingindo velocidade máxima de 115 km / h em corridas de até quatrocentos ou quinhentos metros.',
         createdAt: '2021-05-22T23:12:11.837Z'
@@ -48,9 +54,17 @@ function alimentarFelinos() {
     if (contador < listaDeFelinos.length) {
 
         let divFelinos = document.createElement('div');
-        divFelinos.classList.add('felinos')
+        divFelinos.classList.add('felinos');
 
+        let divId = document.createElement('div');
+        divId.id = listaDeFelinos[contador].id;
+        divId.classList.add('divId');
+        
+        divContainerFelinos.appendChild(divId);
         divContainerFelinos.appendChild(divFelinos);
+        
+
+        
 
         let title = document.createElement('h2');
         title.innerHTML = listaDeFelinos[contador].title;  
@@ -58,9 +72,15 @@ function alimentarFelinos() {
 
         let felinoAlimentado = document.querySelector('h3.felinoAlimentado')
         if(contador<listaDeFelinos.length && contador>0){
-            felinoAlimentado.innerHTML += " - ";
+            felinoAlimentado.innerHTML += "  ";
         }
-        felinoAlimentado.innerHTML += listaDeFelinos[contador].title;
+
+        let linkFelino = document.createElement("a")
+        linkFelino.href = "#" + listaDeFelinos[contador].id;
+        linkFelino.classList.add('linkFelino');
+        linkFelino.innerHTML += listaDeFelinos[contador].id;
+        
+        felinoAlimentado.appendChild(linkFelino);
         
         let imgUrl = document.createElement("img");
         imgUrl.src = listaDeFelinos[contador].imgUrl;
@@ -72,7 +92,7 @@ function alimentarFelinos() {
         let createdAt = document.createElement("h6");
         createdAt.innerHTML = listaDeFelinos[contador].createdAt;
 
-
+        
         divFelinos.appendChild(title);
         divFelinos.appendChild(imgUrl);
         divFelinos.appendChild(description);
